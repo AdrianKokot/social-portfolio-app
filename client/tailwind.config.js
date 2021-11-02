3dee788602da85d04f02de3911c194e200a9b264
Adrian Kokot <adrian.kokot@bbraun.com>
2021-11-02 14:33:12 +0100

    Create angular client project with tailwindcss setup

---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{html,ts}'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography')
+  ],
+};
