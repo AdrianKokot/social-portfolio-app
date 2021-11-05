2f7f9e03f5a7d1c2cbf733e76a93b40252b4b44b
Adrian Kokot <splateusz@gmail.com>
2021-11-05 16:37:23 +0100

    Update angular to version 13

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,17 @@
 module.exports = {
-  // mode: 'jit',
-  purge: ['./src/**/*.{html,ts}'],
-  darkMode: false, // or 'media' or 'class'
+  mode: 'jit',
+  purge: ['./projects/**/src/**/*.{html,ts}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/aspect-ratio')
-,require('@tailwindcss/forms')
-,require('@tailwindcss/typography')
-],
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography')
+  ],
 };
