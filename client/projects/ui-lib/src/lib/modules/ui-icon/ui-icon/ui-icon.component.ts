ff76b0349840aaafed800ab78643770372ce1645
Adrian Kokot <splateusz@gmail.com>
2021-11-17 22:49:48 +0100

    Refactor adding classes to ui-icon component

--- client/projects/ui-lib/src/lib/modules/ui-icon/ui-icon/ui-icon.component.ts
+++ client/projects/ui-lib/src/lib/modules/ui-icon/ui-icon/ui-icon.component.ts
@@ -1,31 +1,47 @@
-import { Component, HostBinding, Input } from '@angular/core';
-import { SafeHtml, DomSanitizer } from '@angular/platform-browser';
+import { Component, HostBinding, Input, ViewEncapsulation } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { uiIcons } from '../icons';
 
 @Component({
   selector: 'ui-icon',
-  styles: [],
+  styles: [
+    `ui-icon {
+      @apply inline-block;
+      @apply focus:outline-none;
+      @apply flex-shrink-0;
+    }`
+  ],
+  encapsulation: ViewEncapsulation.None,
   template: `
-  <svg xmlns="http://www.w3.org/2000/svg"
-     class="w-full h-full fill-current"
-     viewBox="0 0 24 24"
-     stroke-width="2"
-     [innerHTML]="icons[icon]">
-  </svg>
-`
+    <svg xmlns="http://www.w3.org/2000/svg"
+         class="w-full h-full fill-current"
+         viewBox="0 0 24 24"
+         stroke-width="2"
+         [innerHTML]="icons[icon]">
+    </svg>
+  `
 })
 export class UIIconComponent {
   private selectedIcon = '';
-  public defaultHostClass = 'inline-block focus:outline-none flex-shrink-0';
-  public defaultSizeClass = 'w-6 h-6';
+  private cssSizeClasses = 'w-6 h-6';
+  private cssClasses = '';
 
-  @HostBinding('class') private hostClass = this.defaultSizeClass + ' ' + this.defaultHostClass;
-
-  @Input() public set class(v: string) {
-    this.hostClass = v + ' ' + this.defaultHostClass;
+  @Input()
+  public set sizeClass(value: string) {
+    this.cssSizeClasses = value;
+    this.updateHostCssClasses();
   }
 
-  @Input() public set icon(icon: string) {
+  @Input()
+  public set class(value: string) {
+    this.cssClasses = value;
+    this.updateHostCssClasses();
+  }
+
+  @HostBinding('class') private hostClass = this.cssSizeClasses;
+
+  @Input()
+  public set icon(icon: string) {
     this.selectedIcon = icon in this.icons ? icon : 'default';
   }
 
@@ -39,6 +55,10 @@ export class UIIconComponent {
     for (const key of Object.keys(uiIcons)) {
       this.icons[key] = this.sanitizer.bypassSecurityTrustHtml(uiIcons[key]);
     }
+  }
+
+  private updateHostCssClasses(): void {
+    this.hostClass = `${this.cssClasses} ${this.cssSizeClasses}`;
   }
 
 }
