9452749ab72a7ea1380c4105d0d3b6c7806110ec
Adrian Kokot <splateusz@gmail.com>
2021-12-25 13:09:19 +0100

    Add infinity scroll to discussion-list

--- client/projects/client-app/src/app/dashboard/modules/discussion/discussion-list/discussion-list.component.ts
+++ client/projects/client-app/src/app/dashboard/modules/discussion/discussion-list/discussion-list.component.ts
@@ -1,6 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostBinding,
+  Input,
+  OnDestroy,
+  OnInit,
+  TrackByFunction,
+  ViewChild
+} from '@angular/core';
 import { DiscussionService } from "../../../../shared/shared/api/discussion.service";
-import { finalize, map, Observable, tap } from "rxjs";
+import { finalize, Subject, Subscription, switchMap, tap } from "rxjs";
 import { Discussion } from "../../../../shared/shared/models/discussion";
 import { DiscussionParams } from "../../../../shared/shared/api/params/discussion.params";
 
@@ -12,33 +22,85 @@ import { DiscussionParams } from "../../../../shared/shared/api/params/discussio
     class: 'block'
   }
 })
-export class DiscussionListComponent implements OnInit {
+export class DiscussionListComponent implements OnInit, AfterViewInit, OnDestroy {
   @Input() communityId: number | string | null = null;
+  public items: Discussion[] = [];
 
-  public items$: Observable<Discussion[]> | null = null;
   public isLoading: boolean = true;
+
+  private subscription: Subscription | null = null;
+  private scroll$ = new Subject<void>();
+  private hasNextPage = true;
+
+  @ViewChild('infinityBar') infinityBar!: ElementRef<HTMLElement>;
+  private observer: IntersectionObserver | null = null;
+
 
   constructor(
     private discussionService: DiscussionService
-  ) {
+  ) {}
+
+  trackById: TrackByFunction<Discussion> = (index: number, item: Discussion) => item.id;
+
+  private tryUnsubscribe() {
+    if (!this.hasNextPage) {
+      this.unsubscribe();
+    }
+  }
+
+  private unsubscribe() {
+    this.subscription?.unsubscribe();
+    this.observer?.disconnect();
+  }
+
+  ngOnDestroy(): void {
+    this.tryUnsubscribe();
   }
 
   ngOnInit(): void {
-    const param: Partial<DiscussionParams> = {};
+    const param: Partial<DiscussionParams> = {
+      communityId: this.communityId !== null ? this.communityId : undefined,
+      orderBy: "score desc",
+      page: 0
+    };
 
-    param.communityId = this.communityId !== null ? this.communityId : undefined;
-    param.orderBy = "score desc";
-
-    this.items$ = this.discussionService.getAll(param)
+    this.subscription = this.scroll$
       .pipe(
-        tap(() => {
-          this.isLoading = true;
-        }),
-        finalize(() => {
-          this.isLoading = false;
-        }),
-        map(x => x.items)
+        switchMap(() => {
+          param.page != undefined && param.page++;
+
+          console.log('scroll');
+
+          return this.discussionService.getAll(param)
+            .pipe(
+              tap(() => {
+                this.isLoading = true;
+              }),
+              finalize(() => {
+                this.isLoading = false;
+              })
+            )
+        })
       )
+      .subscribe({
+        next: (res) => {
+
+          this.hasNextPage = res.hasNextPage;
+          this.items.push(...res.items);
+
+          console.log(this.items);
+
+          this.tryUnsubscribe();
+        }
+      })
+  }
+
+  ngAfterViewInit(): void {
+    this.observer = new IntersectionObserver(([entry]) => {
+      entry.isIntersecting && this.scroll$.next();
+    });
+
+    this.observer.observe(this.infinityBar.nativeElement);
   }
 
 }
