265a9e038ddb37e0793db8601dd30b7e5204eed2
Adrian Kokot <splateusz@gmail.com>
2021-12-25 11:14:26 +0100

    Prevent angular router scrolling when changing only queryparam in route

--- client/projects/client-app/src/app/app-routing.module.ts
+++ client/projects/client-app/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Router, RouterModule, Routes, Scroll } from '@angular/router';
 import { GuestGuard } from "./shared/shared/auth/guards/guest.guard";
 import { AuthGuard } from "./shared/shared/auth/guards/auth.guard";
 import { LogoutComponent } from "./shared/shared/auth/components/logout/logout.component";
+import { filter, pairwise } from "rxjs";
+import { ViewportScroller } from "@angular/common";
 
 const routes: Routes = [
   {path: '', redirectTo: 'landing', pathMatch: 'full'},
@@ -29,8 +31,32 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: "enabled"})],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
+  constructor(router: Router, viewportScroller: ViewportScroller) {
+    router.events.pipe(
+      filter(e => e instanceof Scroll),
+      pairwise()
+    ).subscribe((e) => {
+      const prev = e[0] as Scroll;
+      const curr = e[1] as Scroll;
+
+      if (curr.position) {
+        viewportScroller.scrollToPosition(curr.position);
+      } else if (curr.anchor) {
+        viewportScroller.scrollToAnchor(curr.anchor);
+      } else {
+        console.log(prev, curr);
+        const prevBase = prev.routerEvent.urlAfterRedirects.split('?')[0];
+        const currBase = curr.routerEvent.urlAfterRedirects.split('?')[0];
+
+        if (prevBase !== currBase) {
+          viewportScroller.scrollToPosition([0, 0]);
+        }
+      }
+
+    });
+  }
 }
